763f50ba2dba37f1e2d1118b5a1dcfb85fc4fc6a
vcombey <vcombey@student.42.fr>
2017-09-04 02:04:12 +0200

    conne chelou et lumiere chelou

--- src/calc.c
+++ src/calc.c
@@ -31,6 +31,20 @@ void	mult_vect(double res[3], double v[3], double t)
 	res[2] = v[2] * t;
 }
 
+void	div_vect(double res[3], double v[3], double t)
+{
+	if (t == 0)
+		return ;
+	res[0] = v[0] / t;
+	res[1] = v[1] / t;
+	res[2] = v[2] / t;
+}
+
+void	normalize(double v[3])
+{
+	div_vect(v, v, sqrt(norme_carre(v)));
+}
+
 void	add_vect(double res[3], double u[3], double v[3])
 {
 	res[0] = u[0] + v[0];
@@ -62,21 +76,29 @@ size_t	calc_lum(double norm_sphere[3])
 	double	lum;
 	size_t intensite_color;
 
-	lum_pos[0] = 2;
-	lum_pos[1] = 2;
+	lum_pos[0] = 3;
+	lum_pos[1] = 1;
 	lum_pos[2] = 1;
 
 	ft_memset(lum_vect, 0, sizeof(double) * 3);
 	sub_vect(lum_vect, norm_sphere, lum_pos);
-	lum = ft_abs_double(scalar_product(lum_vect, norm_sphere));
+	normalize(lum_vect);
+	printf("\nnorm_sphere x %f, y %f, z %f\n", norm_sphere[0], norm_sphere[1], norm_sphere[2]);
+//	normalize(norm_sphere); // pk norm-sphere n est pas de norme rayon ?
+	lum = scalar_product(lum_vect, norm_sphere);
+	if (lum > 0)
+		return (0x001000);
+	lum = ft_abs_double(lum);
 			printf("lum %f\n", lum);
-	intensite_color = (1 - (lum / 40.0)) * 255;
+	intensite_color = lum * 255;
 			printf("intensite_color %zu\n", intensite_color);
+	if (intensite_color < 10)
+		intensite_color = 10;
 	intensite_color <<= 8;
 	return (intensite_color);
 }
 
-size_t	throw_ray(t_cam cam, double ray[3])
+size_t	calc_plan(t_cam cam, double ray[3])
 {
 	double	rayon;
 	double	delta;
@@ -84,6 +106,78 @@ size_t	throw_ray(t_cam cam, double ray[3])
 	double	b;
 	double	c;
 	double	t;
+	double	dist;
+	double	norm_cone[3];
+	double	alpha = 0.7;
+	double	tan_alpha_carre = tan(alpha) * tan(alpha);
+
+	ft_memset(norm_cone, 0, sizeof(double) * 3);
+	rayon = 1;
+	a = ray[0] * ray[0] + ray[1] * ray[1] - ray[2] * ray[2] * tan_alpha_carre;
+	b = 2 * cam.pos[0] * ray[0] + 2 * cam.pos[1] * ray[1] - 2 * cam.pos[2] * ray[2] * tan_alpha_carre;
+	c = cam.pos[0] * cam.pos[0] + cam.pos[1] * cam.pos[1] - cam.pos[2] * cam.pos[2] * tan_alpha_carre;
+
+	delta = calc_delta(a, b, c);
+	//printf("a %f, b %f, c %f, delta %f\n", a, b, c, delta);
+	if (delta < 0)
+		return (0x0);
+	t = ft_min((-b - sqrt(delta)) / (2 * a), (-b + sqrt(delta)) / (2 * a));
+
+	mult_vect(norm_cone, ray, t);
+	dist = norme_carre(norm_cone);
+	add_vect(norm_cone, norm_cone, cam.pos);
+
+	if (norm_cone[2] < 0)
+		norm_cone[2] = -norm_cone[2];
+//  return (0xFF0000);
+	return (calc_lum(norm_cone));
+}
+
+size_t	calc_cone(t_cam cam, double ray[3])
+{
+	double	rayon;
+	double	delta;
+	double	a;
+	double	b;
+	double	c;
+	double	t;
+	double	dist;
+	double	norm_cone[3];
+	double	alpha = 0.7;
+	double	tan_alpha_carre = tan(alpha) * tan(alpha);
+
+	ft_memset(norm_cone, 0, sizeof(double) * 3);
+	rayon = 1;
+	a = ray[0] * ray[0] + ray[1] * ray[1] - ray[2] * ray[2] * tan_alpha_carre;
+	b = 2 * cam.pos[0] * ray[0] + 2 * cam.pos[1] * ray[1] - 2 * cam.pos[2] * ray[2] * tan_alpha_carre;
+	c = cam.pos[0] * cam.pos[0] + cam.pos[1] * cam.pos[1] - cam.pos[2] * cam.pos[2] * tan_alpha_carre;
+
+	delta = calc_delta(a, b, c);
+	//printf("a %f, b %f, c %f, delta %f\n", a, b, c, delta);
+	if (delta < 0)
+		return (0x0);
+	t = ft_min((-b - sqrt(delta)) / (2 * a), (-b + sqrt(delta)) / (2 * a));
+
+	mult_vect(norm_cone, ray, t);
+	dist = norme_carre(norm_cone);
+	add_vect(norm_cone, norm_cone, cam.pos);
+
+	if (norm_cone[2] < 0)
+		norm_cone[2] = -norm_cone[2];
+//  return (0xFF0000);
+	return (calc_lum(norm_cone));
+
+}
+
+size_t	calc_cylindre(t_cam cam, double ray[3])
+{
+	double	rayon;
+	double	delta;
+	double	a;
+	double	b;
+	double	c;
+	double	t;
+	double	dist;
 	double	norm_sphere[3];
 
 	ft_memset(norm_sphere, 0, sizeof(double) * 3);
@@ -96,8 +190,37 @@ size_t	throw_ray(t_cam cam, double ray[3])
 	//printf("a %f, b %f, c %f, delta %f\n", a, b, c, delta);
 	if (delta < 0)
 		return (0x0);
-	t = ft_min((b - sqrt(delta)) / (2 * a), (b + sqrt(delta)) / (2 * a));
+	t = ft_min((-b - sqrt(delta)) / (2 * a), (-b + sqrt(delta)) / (2 * a));
 	mult_vect(norm_sphere, ray, t);
+	dist = norme_carre(norm_sphere);
+	add_vect(norm_sphere, norm_sphere, cam.pos);
+	return (calc_lum(norm_sphere));
+}
+
+size_t	calc_sphere(t_cam cam, double ray[3])
+{
+	double	rayon;
+	double	delta;
+	double	a;
+	double	b;
+	double	c;
+	double	t;
+	double	dist;
+	double	norm_sphere[3];
+
+	ft_memset(norm_sphere, 0, sizeof(double) * 3);
+	rayon = 1;
+	a = norme_carre(ray);
+	b = 2 * scalar_product(ray, cam.pos);
+	c = norme_carre(cam.pos) - rayon * rayon;
+
+	delta = calc_delta(a, b, c);
+	//printf("a %f, b %f, c %f, delta %f\n", a, b, c, delta);
+	if (delta < 0)
+		return (0x0);
+	t = ft_min((-b - sqrt(delta)) / (2 * a), (-b + sqrt(delta)) / (2 * a));
+	mult_vect(norm_sphere, ray, t);
+	dist = norme_carre(norm_sphere);
 	add_vect(norm_sphere, norm_sphere, cam.pos);
 	return (calc_lum(norm_sphere));
 }
@@ -127,7 +250,7 @@ void	calc(t_env *env)
 
 	norm_vert[0] = 0;
 	norm_vert[1] = 0;
-	norm_vert[2] = 1;
+	norm_vert[2] = -1;
 
 	norm_hor[0] = 0;
 	norm_hor[1] = 1;
@@ -154,7 +277,7 @@ void	calc(t_env *env)
 			//printf("coef %f", coef);
 			ray[1] += coef * norm_hor[1];
 			//printf("\nray %f, %f, %f\n", ray[0], ray[1], ray[2]);
-			ft_pixelput(env, pix_hor, pix_vert, throw_ray(cam, ray));
+			ft_pixelput(env, pix_hor, pix_vert, calc_sphere(cam, ray));
 			pix_hor++;
 		}
 		pix_vert++;
--- src/env.c
+++ src/env.c
@@ -1,4 +1,5 @@
 #include "rt.h"
+#include "libft.h"
 #include <unistd.h>
 #include <mlx.h>
 
@@ -8,13 +9,13 @@ int		init_env(t_env *env)
 	env->img = NULL;
 	env->mlx = NULL;
 	if (!(env->mlx = mlx_init()))
-		return (fatal("mlx problem"));
+		fatal("mlx problem");
 	if (!(env->win = mlx_new_window(env->mlx, SCREEN_WIDTH,
 	SCREEN_HEIGHT, "rt")))
-		return (fatal("mlx problem"));
+		fatal("mlx problem");
 	if (!(env->img = mlx_new_image(env->mlx, SCREEN_WIDTH,
 	SCREEN_HEIGHT)))
-		return (fatal("mlx problem"));
+		fatal("mlx problem");
 	env->ptr = mlx_get_data_addr(env->img, &env->bpp, &env->size_line,
 	&env->endian);
 	return (0);
