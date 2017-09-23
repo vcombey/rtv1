c8f31b7eb56ec6de84e92321459e1f694936ab60
vcombey <vcombey@student.42.fr>
2017-09-23 11:12:57 +0200

    rotation ok

--- src/key_event.c
+++ src/key_event.c
@@ -40,14 +40,14 @@ void	rot_left(t_cam *cam)
 
 static void	rot_arround_left(t_cam *cam)
 {
-	cam->pos = rot(cam->pos, -TETA);
-	cam->dir = rot(cam->dir, -TETA);
+	cam->pos = rot(cam->pos, -TETA * 3);
+	cam->dir = rot(cam->dir, -TETA * 3);
 }
 
 static void	rot_arround_right(t_cam *cam)
 {
-	cam->pos = rot(cam->pos, TETA);
-	cam->dir = rot(cam->dir, TETA);
+	cam->pos = rot(cam->pos, TETA * 3);
+	cam->dir = rot(cam->dir, TETA * 3);
 }
 
 cl_float3	vectorial_product(cl_float3 u, cl_float3 v)
@@ -84,25 +84,29 @@ void	init_norm_cam_dir(cl_float3 *norm_vert, cl_float3 *norm_hor, cl_float3 dir)
 {
 	dir = normalize(dir);
 //	printf("dir %f, %f, %f\n", dir.x, dir.y,dir.z); 
-	if (dir.y != 0)
-	{
-		norm_hor->z = 0;
-		norm_hor->x = sqrt(dir.y * dir.y / (dir.x * dir.x + dir.y * dir.y));
-		norm_hor->y = -dir.x / dir.y * norm_hor->x;
-		if (norm_hor->y < 0)
-			norm_hor->y *= -1;
-	}
-	else if (dir.x != 0)
-	{
-		norm_hor->z = 0;
-		norm_hor->y = sqrt(dir.x * dir.x / (dir.x * dir.x + dir.y * dir.y));
-		if (norm_hor->y < 0)
-			norm_hor->y *= -1;
-		norm_hor->x = -dir.y / dir.x * norm_hor->y;
-	}
-	*norm_hor = normalize(*norm_hor);
-	*norm_vert = vectorial_product(*norm_hor, dir);
-	*norm_vert = normalize(*norm_vert);
+	*norm_hor = rot(dir, -M_PI / 2);
+	*norm_vert =  (cl_float3){.x = 0, .y = 0, .z = 1};
+	/*
+**		if (dir.y != 0)
+**		{
+**			norm_hor->z = 0;
+**			norm_hor->x = sqrt(dir.y * dir.y / (dir.x * dir.x + dir.y * dir.y));
+**			norm_hor->y = -dir.x / dir.y * norm_hor->x;
+**			if (norm_hor->y < 0)
+**				norm_hor->y *= -1;
+**		}
+**		else if (dir.x != 0)
+**		{
+**			norm_hor->z = 0;
+**			norm_hor->y = sqrt(dir.x * dir.x / (dir.x * dir.x + dir.y * dir.y));
+**			if (norm_hor->y < 0)
+**				norm_hor->y *= -1;
+**			norm_hor->x = -dir.y / dir.x * norm_hor->y;
+**		}
+**		*norm_hor = normalize(*norm_hor);
+**		*norm_vert = vectorial_product(*norm_hor, dir);
+**		*norm_vert = normalize(*norm_vert);
+*/
 //	printf("hor %f, %f, %f\n", norm_hor->x, norm_hor->y,norm_hor->z); 
 //	printf("vert %f, %f, %f\n", norm_vert->x, norm_vert->y,norm_vert->z); 
 }
